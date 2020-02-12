57850bb4d1e6ea5eee4208add89b7bd92f9da453
Amith Pasam <56844550+amithpasam98@users.noreply.github.com>
2020-02-12 11:26:38 -0500

    Add files via upload

---
+++ backend-Spring/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'amith', 'JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'amith', 'Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'amith', 'Microservices', sysdate(), false);
